5a1ebea4cb9b080d30682566ae98e9cca6376818
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2021-10-06 15:52:14 +0530

    Home page progress-1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import './header.css'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
 
 const Header = () => {
     return (
         <div>
-            <h1>This is Header</h1>
+            <Navbar className="nav-whole">
+                <Container className="nav-c">
+                    <Navbar.Brand href="a" className="nav-brand">Tripti</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav className="nav">
+                            <Nav.Link href="a" className="nav-txt-rd">Add Food</Nav.Link>
+                            <Nav.Link href="a" className="nav-txt-rd">Need Food</Nav.Link>
+                            <Nav.Link href="a" className="nav-txt-rd">Causes worth donating</Nav.Link>
+                            <Nav.Link href="a" className="nav-txt-rd">NGOs</Nav.Link>
+                            <Nav.Link href="a" className="nav-txt-rd">Government Schemes</Nav.Link>
+                            <Nav.Link href="a" className="nav-txt-rd">Give them a treat</Nav.Link>
+                            <Nav.Link href="a" className="nav-txt-rd">For restaurants</Nav.Link>
+                            <Button className="btn-login">Login</Button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './home.css'
+import home1 from '../../media/Home.gif'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const Home = () => {
     return (
         <div>
-            <h1>This is Homepage</h1>
+            <Container>
+            <Row>
+                <Col className="home-left">
+                    <h1 className="home-h">WELCOME</h1>
+                    <p className="home-txt">We strongly believe that food is a <strong>Necessity</strong> and not a <strong>luxury</strong></p>
+                    <p className="home-txt1">We are glad that you joined us in our mission to <strong>#FeedIndia</strong></p>
+                </Col>
+                <Col>
+                    <img src={home1} alt="img" className="home-img1"></img>
+                </Col>
+            </Row>
+            </Container>
         </div>
     )
 }
